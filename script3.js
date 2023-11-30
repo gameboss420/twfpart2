2e403d5656dab8ab0620a1ee9b0811b9b98767e9
gameboss420 <130771671+gameboss420@users.noreply.github.com>
2023-11-30 10:55:36 -0600

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-	// MM - DD - YYYY
+  // MM - DD - YYYY
   function getDayFromDate(dateString) {
     var now = new Date(dateString);
     var start = new Date(now.getFullYear(), 0, 0);
@@ -7,12 +7,12 @@ $(document).ready(() => {
     var oneDay = 1000 * 60 * 60 * 24;
     return Math.floor(diff / oneDay);
   }
-  
-  let cutoffLow = getDayFromDate("11/24");
-  let cutoffHigh = getDayFromDate("12/31");
+
+  let cutoffLow = getDayFromDate("1/01");
+  let cutoffHigh = getDayFromDate("1/05");
   let now = getDayFromDate(new Date());
-    
+
   if(now >= cutoffLow && now <= cutoffHigh) {
-  	document.querySelector('#two').style.display = "block";
+    document.querySelector('#two').style.display = "block";
   }
 });
